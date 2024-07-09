0cdba6f2192b70db928beef84580f638a8b770e6
YashDEVP <dinesh1328gupta@gmail.com>
2024-07-09 10:56:01 -0700

    Modification in Controller code adding service layer

--- src/main/java/com/derivativemarket/posttrade/org/controllers/PostTradeController.java
+++ src/main/java/com/derivativemarket/posttrade/org/controllers/PostTradeController.java
@@ -3,6 +3,7 @@ package com.derivativemarket.posttrade.org.controllers;
 import com.derivativemarket.posttrade.org.dto.TradeDTO;
 import com.derivativemarket.posttrade.org.entities.TradeEntity;
 import com.derivativemarket.posttrade.org.repositories.MarketRepository;
+import com.derivativemarket.posttrade.org.services.MarketService;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -16,10 +17,10 @@ import java.util.List;
 @RequestMapping(path="/trades")
 public class PostTradeController {
 
-    private final MarketRepository marketRepository;
+    private final MarketService marketService;
 
-    public PostTradeController(MarketRepository marketRepository){
-        this.marketRepository=marketRepository;
+    public PostTradeController(MarketService marketService){
+        this.marketService=marketService;
     }
 
 
@@ -27,23 +28,23 @@ public class PostTradeController {
     @RequestParam optional parameter then it will work
     */
     @GetMapping("/{ticketId}")
-    public TradeEntity getTradeByTicketId(@PathVariable Long id){
-       return marketRepository.findById(id).orElse(null);
+    public TradeDTO getTradeByTicketId(@PathVariable Long id){
+       return marketService.getTradeByTicketId(id);
         // return new TradeDTO(1,ticketId,"Pimco","DBS",1000000,"IRS"
             //    , LocalDate.of(2024,7,8),"New");
     }
 
     /*get all the employee in sorted order by date*/
     @GetMapping()
-    public List<TradeEntity> getAllTrades(@RequestParam(required = false, name ="inputAge") LocalDate date,@RequestParam(required = false)String SortBy){
-        return marketRepository.findAll();
+    public List<TradeDTO> getAllTrades(@RequestParam(required = false, name ="inputAge") LocalDate date,@RequestParam(required = false)String SortBy){
+        return marketService.getAllTrades();
     }
 
     /*browser always interact with get request and not interact with post request.
     @RequestBody when we need to pass whole json obj as parameter
      */
     @PostMapping
-    public TradeEntity insertTrade(@RequestBody TradeEntity inputTrade){
-        return marketRepository.save(inputTrade);
+    public TradeDTO insertTrade(@RequestBody TradeDTO inputTrade){
+        return marketService.insertTrade(inputTrade);
     }
 }
