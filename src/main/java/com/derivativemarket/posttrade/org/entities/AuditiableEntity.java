13dfec577eef751fcbfd46c4e5018de9c39fed9e
YashDEVP <dinesh1328gupta@gmail.com>
2024-07-19 23:11:02 -0700

    Added Auditing Feature For DB

---
+++ src/main/java/com/derivativemarket/posttrade/org/entities/AuditiableEntity.java
@@ -0,0 +1,36 @@
+package com.derivativemarket.posttrade.org.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+@EntityListeners(AuditingEntityListener.class) /* it gives feature of audit how create update the object and when it was updated*/
+@Getter
+@Setter
+@MappedSuperclass
+public class AuditiableEntity {
+
+    /* All four field is part of auditing EntityListeners(AuditingEntityListener.class)*/
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    private String updatedBy;
+
+}
