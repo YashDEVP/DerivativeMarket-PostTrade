05958fe5600cf4d9e49f49959572120e0ed7a20d
YashDEVP <dinesh1328gupta@gmail.com>
2024-07-21 01:49:47 -0700

    add logging

--- src/main/java/com/derivativemarket/posttrade/org/controllers/PostTradeController.java
+++ src/main/java/com/derivativemarket/posttrade/org/controllers/PostTradeController.java
@@ -3,8 +3,12 @@ package com.derivativemarket.posttrade.org.controllers;
 import com.derivativemarket.posttrade.org.dto.TradeDTO;
 import com.derivativemarket.posttrade.org.entities.TradeEntity;
 import com.derivativemarket.posttrade.org.exception.ResourceNotFoundException;
+import com.derivativemarket.posttrade.org.repositories.MarketRepository;
 import com.derivativemarket.posttrade.org.services.MarketService;
+import jakarta.annotation.Resource;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 /*Contains @Controller and @ResponseBody
@@ -21,6 +26,9 @@ import java.util.Optional;
 @RestController
 @RequestMapping(path="/trades")
 public class PostTradeController {
+
+
+    Logger logger= LoggerFactory.getLogger(PostTradeController.class);
 
     private final MarketService marketService;
 
@@ -35,10 +43,16 @@ public class PostTradeController {
     /*refId in GetMapping and refId should have same name otherwise it will give exception*/
     @GetMapping("/{refId}")
     public ResponseEntity<TradeDTO> getTradeByTicketId(@PathVariable(name="refId") Long id){
-       Optional<TradeDTO> tradeDTO=marketService.getTradeByTicketId(id);
-      return tradeDTO
-              .map(tradeDTO1 -> ResponseEntity.ok(tradeDTO1))
-              .orElseThrow(()->new ResourceNotFoundException("Trade was not found"));
+        try {
+            logger.trace("Starting of getTradeByTicketId Controller");
+            Optional<TradeDTO> tradeDTO = marketService.getTradeByTicketId(id);
+            return tradeDTO
+                    .map(tradeDTO1 -> ResponseEntity.ok(tradeDTO1))
+                    .orElseThrow(() -> new ResourceNotFoundException("Trade was not found"));
+        }catch(Exception ex){
+            logger.error("Exception: getTradeByTicketId Controller : " + ex);
+            throw new RuntimeException(ex);
+        }
     }
 
 //    @ExceptionHandler(NoSuchElementException.class)
@@ -49,8 +63,14 @@ public class PostTradeController {
     /*get all the employee in sorted order by date*/
     @GetMapping()
     public ResponseEntity<List<TradeDTO>> getAllTrades(@RequestParam(required = false, name ="inputAge") LocalDate date,@RequestParam(required = false)String SortBy){
-        List<TradeDTO> tradesDTO= marketService.getAllTrades();
-        return ResponseEntity.ok(tradesDTO);
+       try {
+           logger.trace("Starting of getAllTrades Controller");
+           List<TradeDTO> tradesDTO = marketService.getAllTrades();
+           return ResponseEntity.ok(tradesDTO);
+       }catch(Exception ex){
+           logger.error("Exception: getAllTrades Controller : " + ex);
+           throw new RuntimeException(ex);
+       }
     }
 
     /*browser always interact with get request and not interact with post request.
@@ -58,27 +78,50 @@ public class PostTradeController {
      */
     @PostMapping
     public ResponseEntity<TradeDTO> insertTrade(@RequestBody @Valid TradeDTO inputTrade){
+        try{
+            logger.trace("Starting of insertTrade Controller");
         TradeDTO tradeDTO= marketService.insertTrade(inputTrade);
         return new ResponseEntity<>(tradeDTO, HttpStatus.CREATED);
+        }catch(Exception ex){
+            logger.error("Exception: insertTrade Controller : " + ex);
+            throw new RuntimeException(ex);
+        }
     }
 
     @PutMapping(path="/{tradeId}")
     public ResponseEntity<TradeDTO> updateTradeById(@RequestBody @Valid TradeDTO tradeDTO,@PathVariable Long tradeId){
-        return ResponseEntity.ok(marketService.updateTradeById(tradeId,tradeDTO));
+        try {
+            logger.trace("Starting of updateTradeById Controller");
+            return ResponseEntity.ok(marketService.updateTradeById(tradeId, tradeDTO));
+        }catch(Exception ex){
+            logger.error("Exception: updateTradeById Controller : " + ex);
+            throw new RuntimeException(ex);
+        }
     }
 
     @DeleteMapping(path="/{refId}")
     public ResponseEntity<Boolean> termination(@PathVariable Long refId){
-       Boolean isTerminated=marketService.termination(refId);
-       if(isTerminated) return ResponseEntity.ok(true);
-       return ResponseEntity.notFound().build();
-
+        try {
+            logger.trace("Starting of termination Controller");
+            Boolean isTerminated = marketService.termination(refId);
+            if (isTerminated) return ResponseEntity.ok(true);
+            return ResponseEntity.notFound().build();
+        }catch(Exception ex){
+            logger.error("Exception: termination Controller : " + ex);
+            throw new RuntimeException(ex);
+        }
     }
 
     @PatchMapping(path="/{refId}")
     public ResponseEntity<TradeDTO> reuploadTrade(@PathVariable Long refId, @RequestBody Map<String,Object> updates){
-        TradeDTO tradeDTO=marketService.reuploadTrade(refId,updates);
-        if(tradeDTO==null) return ResponseEntity.notFound().build();
-        return ResponseEntity.ok(tradeDTO);
+        try {
+            logger.trace("Starting of reuploadTrade Controller");
+            TradeDTO tradeDTO = marketService.reuploadTrade(refId, updates);
+            if (tradeDTO == null) return ResponseEntity.notFound().build();
+            return ResponseEntity.ok(tradeDTO);
+        }catch(Exception ex){
+            logger.error("Exception: reuploadTrade Controller : " + ex);
+            throw new RuntimeException(ex);
+        }
     }
 }
