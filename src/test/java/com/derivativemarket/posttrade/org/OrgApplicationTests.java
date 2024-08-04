1f543286219625d23952043045b22a1dca0f057f
YashDEVP <dinesh1328gupta@gmail.com>
2024-08-03 23:27:05 -0700

    testing JWT token

--- src/test/java/com/derivativemarket/posttrade/org/OrgApplicationTests.java
+++ src/test/java/com/derivativemarket/posttrade/org/OrgApplicationTests.java
@@ -1,13 +1,28 @@
 package com.derivativemarket.posttrade.org;
 
+import com.derivativemarket.posttrade.org.entities.CompanyEntity;
+import com.derivativemarket.posttrade.org.services.JwtService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class OrgApplicationTests {
 
+	@Autowired
+	private JwtService jwtService;
+
 	@Test
 	void contextLoads() {
-	}
+		CompanyEntity companyEntity=new CompanyEntity(4L,"rooshad@derivativemarkit.com","password123","ABC.OPS","5040","ABC Capital");
+		String token=jwtService.generateToken(companyEntity);
+
+		System.out.println(token);
+
+		Long id=jwtService.getUserIdFromToken(token);
+
+		System.out.println(id);
+}
 
 }
+
