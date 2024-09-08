aaf58a4ee6aaaef57cd31088cbd3131667519ffd
YashDEVP <dinesh1328gupta@gmail.com>
2024-09-08 07:54:05 -0700

    Remove Error related to JWT Token

--- src/main/java/com/derivativemarket/posttrade/org/dto/LoginDTO.java
+++ src/main/java/com/derivativemarket/posttrade/org/dto/LoginDTO.java
@@ -1,10 +1,12 @@
 package com.derivativemarket.posttrade.org.dto;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Data
 public class LoginDTO {
     String companyEmail;
     String password;
