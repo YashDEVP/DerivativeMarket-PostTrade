0d714054e7b074e6e79f2151cb9ac463bff84281
YashDEVP <dinesh1328gupta@gmail.com>
2024-09-09 21:52:31 -0700

    Added acess token and refresh token concept

--- src/main/java/com/derivativemarket/posttrade/org/controllers/AuthController.java
+++ src/main/java/com/derivativemarket/posttrade/org/controllers/AuthController.java
@@ -2,6 +2,7 @@ package com.derivativemarket.posttrade.org.controllers;
 
 
 import com.derivativemarket.posttrade.org.dto.LoginDTO;
+import com.derivativemarket.posttrade.org.dto.LoginResponseDTO;
 import com.derivativemarket.posttrade.org.dto.SignUpDTO;
 import com.derivativemarket.posttrade.org.dto.CompanyDTO;
 import com.derivativemarket.posttrade.org.services.AuthService;
@@ -11,8 +12,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
 
 @RestController
 @RequestMapping("/auth")
@@ -21,6 +26,9 @@ public class AuthController {
     private final CompanyService companyService;
     private final AuthService authService;
 
+    @Value("${deploy.env}")
+    private String deployEnv;
+
     @PostMapping("/signup")
     public ResponseEntity<CompanyDTO> signUp(@RequestBody SignUpDTO signUpDTO){
         CompanyDTO companyDTO=companyService.signUp(signUpDTO);
@@ -28,11 +36,22 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO, HttpServletRequest request, HttpServletResponse response){
-        String token= authService.logIn(loginDTO);
-        Cookie cookie=new Cookie("token",token);
-        cookie.setHttpOnly(true); //noone can access it onlybe access by http
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginDTO loginDTO, HttpServletRequest request, HttpServletResponse response){
+        LoginResponseDTO token= authService.logIn(loginDTO);
+        Cookie cookie=new Cookie("refreshToken",token.getRefreshToken());
+        cookie.setHttpOnly(true);//noone can access it onlybe access by http
+        cookie.setSecure("production".equals(deployEnv)); // noone can update the cookie in middle we have to only use have to only use when we are in production env
         response.addCookie(cookie);
         return ResponseEntity.ok(token);
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<LoginResponseDTO> refresh(HttpServletRequest request){
+        String refreshToken =Arrays.stream(request.getCookies()).
+                filter(cookie -> "refreshToken".equals(cookie.getName()))
+                .map(Cookie::getValue)
+                .findFirst().orElseThrow(()-> new AuthenticationServiceException("Refresh token not found inside the cookie"));
+        LoginResponseDTO loginResponseDTO=  authService.refreshToken(refreshToken)  ;
+        return ResponseEntity.ok(loginResponseDTO);
     }
 }
--- src/main/java/com/derivativemarket/posttrade/org/services/AuthService.java
+++ src/main/java/com/derivativemarket/posttrade/org/services/AuthService.java
@@ -1,6 +1,7 @@
 package com.derivativemarket.posttrade.org.services;
 
 import com.derivativemarket.posttrade.org.dto.LoginDTO;
+import com.derivativemarket.posttrade.org.dto.LoginResponseDTO;
 import com.derivativemarket.posttrade.org.entities.CompanyEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -14,13 +15,23 @@ public class AuthService {
 
     private  final AuthenticationManager authenticationManager;
     private  final JwtService jwtService;
+    private final CompanyService companyService;
 
-    public String logIn(LoginDTO loginDTO) {
+    public LoginResponseDTO logIn(LoginDTO loginDTO) {
         Authentication authentication=authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginDTO.getCompanyEmail(),loginDTO.getPassword())
         );
         CompanyEntity companyEntity=(CompanyEntity) authentication.getPrincipal();
-        String token=jwtService.generateToken(companyEntity);
-        return token;
+        String accessToken=jwtService.generateAccessToken(companyEntity);
+        String refreshToken=jwtService.generateRefreshToken(companyEntity);
+        return new LoginResponseDTO(companyEntity.getId(),accessToken,refreshToken);
+    }
+
+    public LoginResponseDTO refreshToken(String refreshToken) {
+        Long companyId=jwtService.getUserIdFromToken(refreshToken);
+        CompanyEntity companyEntity=companyService.getCompanyById(companyId);
+        String accessToken=jwtService.generateAccessToken(companyEntity);
+        return new LoginResponseDTO(companyEntity.getId(),accessToken,refreshToken);
+
     }
 }
